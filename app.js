4635328dbcf9818dab17ce764f623c98b781b1c2
Dhrubo <samiul15-13051@diu.edu.bd>
2021-06-08 21:13:15 +0600

    Handling Post rwequest

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+app.use(express.json());
+
 //Assigning Port
 const port = 3000
 
@@ -19,7 +21,19 @@ app.get('/api/students', (req, res) => {
   })
 });
 
+//Making Post Request
+app.post('/api/students', (req, res) => {
+  const student = req.body;
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    const students = JSON.parse(data);
+    students.students.push(student);
+    fs.writeFile('./db.json', JSON.stringify(students), (err) => {
+      res.send(student);
+    });
+  });
+});
+
 //Creating Listener
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
