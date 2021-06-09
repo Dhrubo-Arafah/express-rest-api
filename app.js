cf88d87c6905d2cbabeb17432988ad54e7db2f9f
Dhrubo <samiul15-13051@diu.edu.bd>
2021-06-09 08:52:25 +0600

    Express Middleware

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 //Express
 const express = require('express');
 const studentRouter = require('./routers/studentRouter')
-
+const morgan = require('morgan')
 const app = express();
-app.use(express.json());
+
+//Defining Built-in Middlewares
+app.use(express.json()); //POST/PUT/PATCH -> JSON Object -> Req
+app.use(express.urlencoded({ extended: true })); // id=1&&name=something
+app.use(express.static('public')); //Handle Static File
 app.use('/api/students', studentRouter);
+
+//Defining Custom Middleware
+app.use((req, res, next) => {
+  console.log("I'm Middleware 1!");
+  next(); //Help to pass middleware to the next middleware
+})
+
+//Defining Third-Party Middleware
+app.use(morgan('dev'));
 
 //Assigning Port
 const port = 3000
-
 
 //Making Get Request
 app.get('/', (req, res) => {
